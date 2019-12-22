6478b8ffb59762c77affead0b01d313c301e0a0b
karthickram286 <karthick.r@zohocorp.com>
2019-12-22 18:09:56 +0530

    exercise routes

--- backend/routes/exercises.routes.js
+++ backend/routes/exercises.routes.js
@@ -1,15 +1,53 @@
 const router = require('express').Router();
 const Exercise = require('../models/exercise.model');
 
+/**
+ * Returns all exercises
+ */
 router.route('/getAllExercises').get(async (req, res) => {
     try {
         let allExercises = await Exercise.find();
-        res.status(200).json(allExercises);
+        if (allExercises) {
+            res.status(200).json(allExercises);
+        } else {
+            res.status(200).json('Exercises not found');
+        }
     } catch(err) {
         res.status(400).json('Error: ' + err);
     }
 });
 
+/**
+ * Returns an Exercise by ID
+ */
+router.route('/getExercise/:id').get(async (req, res) => {
+    try {
+        let exercise = await Exercise.findById(req.params.id);
+        if (exercise) {
+            res.status(200).json(exercise);
+        } else {
+            res.status(200).json('Exercise not found');
+        }
+    } catch (err) {
+        res.status(400).json('Error: ' + err);
+    }
+});
+
+/**
+ * Deletes an Exercise by ID
+ */
+router.route('/deleteExercise/:id').delete(async (req, res) => {
+    try {
+        await Exercise.findByIdAndRemove(req.params.id); 
+        res.status(200).json('Exercise deleted');
+    } catch (err) {
+        res.status(400).json('Error: ' + err);
+    }
+});
+
+/**
+ * Adds an Exercise
+ */
 router.route('/addExercise').post(async (req, res) => {
     try {
         const userName = req.body.userName;
@@ -28,6 +66,25 @@ router.route('/addExercise').post(async (req, res) => {
         res.status(200).json('Exercise saved!');
 
     } catch (err) {
+        res.status(400).json('Error: ' + err);
+    }
+});
+
+/**
+ * Update an exercise by ID
+ */
+router.route('/updateExercise/:id').put(async (req, res) => {
+    try {
+        await Exercise.findByIdAndUpdate(req.params.id, {
+            $set: {
+                userName: req.body.userName,
+                description: req.body.description,
+                duration: req.body.duration,
+                date: req.body.date
+            }
+        }, { useFindAndModify: false });
+        res.status(200).json('Exercise updated!');
+    } catch(err) {
         res.status(400).json('Error: ' + err);
     }
 });
